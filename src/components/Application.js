54075668a8e42426f05becdacb239fdcb31fcc22
Brady Blair <bradyblair@Bradys-MacBook-Pro.local>
2021-02-16 13:23:31 -0500

    remove hard coded data from app and use api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DayList from '../components/DayList';
 import Appointment from '../components/Appointment';
 import axios from 'axios';
 
 import 'components/Application.scss';
-
-const days = [
-  {
-    id: 1,
-    name: 'Monday',
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: 'Tuesday',
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: 'Wednesday',
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -71,7 +53,16 @@ const appointments = [
 ];
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
   const [day, updateDay] = useState('Monday');
+
+  useEffect(() => {
+    const url = 'http://localhost:8001/api/days';
+    axios.get(url).then((res) => {
+      setDays(res.data);
+    });
+  }, []);
+
   return (
     <main className='layout'>
       <section className='sidebar'>
