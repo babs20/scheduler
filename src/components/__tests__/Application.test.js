d3c4c32e45c67343dbcbba6c49a0bf389216f609
babs20 <thevillemc@gmail.com>
2021-02-22 12:47:24 -0500

    add mocks for axios library and add tests for application

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,28 @@
-import React from "react";
-
-import { render, cleanup } from "@testing-library/react";
-
-import Application from "components/Application";
+import React from 'react';
+import { render, cleanup, waitForElement } from '@testing-library/react';
+import Application from 'components/Application';
+import { fireEvent } from '@testing-library/react/dist';
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+describe('Application', () => {
+  it('changes the schedule when a new day is selected', () => {
+    const { getByText } = render(<Application />);
+
+    return waitForElement(() => getByText('Monday')).then(() => {
+      fireEvent.click(getByText('Tuesday'));
+
+      expect(getByText('Leopold Silvers')).toBeInTheDocument();
+    });
+  });
+
+  it('changes the schedule when a new day is selected', async () => {
+    const { getByText } = render(<Application />);
+
+    await waitForElement(() => getByText('Monday'));
+
+    fireEvent.click(getByText('Tuesday'));
+
+    expect(getByText('Leopold Silvers')).toBeInTheDocument();
+  });
 });
